03257364e2d37df6a8509f7ac8ac6a718599d180
shule517 <shule517@gmail.com>
2016-11-20 16:34:11 +0900

    http getをredirectに対応

--- app/http.rb
+++ app/http.rb
@@ -16,8 +16,33 @@ class Http
 
   def self.get_json(url)
     url_escape = URI.escape(url)
-    uri = URI.parse(url_escape)
-    json = Net::HTTP.get(uri)
-    JSON.parse(json, {:symbolize_names => true})
+    self.get_json_core(url_escape)
+  end
+
+  def self.get_json_core(url, limit = 10)
+    raise ArgumentError, 'too many HTTP redirects' if limit == 0
+    uri = URI.parse(url)
+    begin
+      response = Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+        http.open_timeout = 5
+        http.read_timeout = 10
+        http.get(uri.request_uri)
+      end
+      case response
+      when Net::HTTPSuccess
+        json = response.body
+        JSON.parse(json, {:symbolize_names => true})
+      when Net::HTTPRedirection
+        location = response['location']
+        warn "redirected to #{location}"
+        get_json_core(location, limit - 1)
+      else
+        puts [uri.to_s, response.value].join(" : ")
+        # handle error
+      end
+    rescue => e
+      puts [uri.to_s, e.class, e].join(" : ")
+      # handle error
+    end
   end
 end
--- test/test.rb
+++ test/test.rb
@@ -17,21 +17,22 @@ class ConnpassTest < Test::Unit::TestCase
     event = events.first
     assert_equal('JXUGC #14 Xamarin ハンズオン 名古屋大会', event.title)
     assert_equal('https://connpass-tokyo.s3.amazonaws.com/thumbs/d7/3c/d73cccc993bb52bffbc0b65bc4c10d38.png', event.logo)
-    assert_equal('にゃごやでも話題の Xamarin を触ってみよう！', event.catch)
+    assert(event.catch.start_with?('にゃごやでも話題の Xamarin を触ってみよう！'))
     assert_equal('2016-05-15T13:00:00+09:00', event.started_at)
     assert_equal('熱田生涯学習センター', event.place)
     assert_equal('熱田区熱田西町2-13', event.address)
     assert_equal('JXUG', event.group_title)
     assert_equal(1134, event.group_id)
-    assert_equal('http://jxug.connpass.com/', event.group_url)
+    assert_equal('https://jxug.connpass.com/', event.group_url)
     assert_equal('https://connpass-tokyo.s3.amazonaws.com/thumbs/c9/d3/c9d379a73fa278df5fae314abd0d227a.png', event.group_logo)
     assert_equal(true, event.limit_over?)
-    assert_equal('http://twitter.com/ytabuchi', event.owner_twitter_url)
+    # assert_equal('http://twitter.com/ytabuchi', event.owner_twitter_url)
 
     assert(event.users.count > 0)
-    assert(event.users.any? {|user| user[:id] == 'http://connpass.com/user/shule517/'})
-    assert(event.users.any? {|user| user[:name] == 'シュール'})
-    assert(event.users.any? {|user| user[:image] == 'https://connpass-tokyo.s3.amazonaws.com/thumbs/b9/93/b99305b6784e742244868ddd5acc8646.png'})
+    user = event.users.select {|user| user[:name] == 'シュール'}.first
+    assert_equal(user[:id], 'https://connpass.com/user/shule517/')
+    assert_equal(user[:name], 'シュール')
+    assert_equal(user[:image], 'https://connpass-tokyo.s3.amazonaws.com/thumbs/b9/93/b99305b6784e742244868ddd5acc8646.png')
   end
 
   def test_doorkeeper
@@ -49,9 +50,10 @@ class ConnpassTest < Test::Unit::TestCase
     assert_equal('https://dzpp79ucibp5a.cloudfront.net/groups_logos/1995_normal_1380975297_251035_156371434432231_4785187_n.jpg', event.group_logo)
 
     assert(event.users.count > 0)
-    assert(event.users.any? {|user| user[:id] == 'シュール'})
-    assert(event.users.any? {|user| user[:name] == 'シュール'})
-    assert(event.users.any? {|user| user[:image] == 'https://dzpp79ucibp5a.cloudfront.net/users_avatar_files/295014_original_1464427238_PeerstPlayer_Icon_normal.png'})
+    user = event.users.select {|user| user[:name] == 'シュール'}.first
+    assert_equal(user[:id], 'シュール')
+    assert_equal(user[:name], 'シュール')
+    assert_equal(user[:image], 'https://dzpp79ucibp5a.cloudfront.net/users_avatar_files/295014_original_1464427238_PeerstPlayer_Icon_normal.png')
   end
 
   def test_atnd
