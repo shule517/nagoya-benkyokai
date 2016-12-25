6ef5d86b5fcffed66775952719d80af96ae3e319
shule517 <shule517@gmail.com>
2016-12-25 17:50:49 +0900

    update model associate

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,4 @@
 class Event < ApplicationRecord
+  has_many :event_users
+  has_many :users, :through => :event_users
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :event_users
+  has_many :events, through => :event_users
 end
