113eccabc6fa7d89aad55f3dc7e65eff055fd203
shule517 <shule517@gmail.com>
2017-01-02 16:57:24 +0900

    update: doorkeeper collect

--- app/services/doorkeeper.rb
+++ app/services/doorkeeper.rb
@@ -5,12 +5,16 @@ require_relative './doorkeeper_event'
 
 class Doorkeeper
   def search(keywords, ym_list = [])
-    search_core(0, keywords, ym_list.first)
+    events = []
+    keywords.take(5).each do |keyword|
+      events += search_core(0, keyword, ym_list.first)
+    end
+    events
   end
 
   private
-  def search_core(start, keywords, ym)
-    url = "https://api.doorkeeper.jp/events/?q=#{keywords.first}&sort=starts_at#{ym.nil? ? "" : "&since=#{ym}01000000"}&page=#{start.to_s}"
+  def search_core(start, keyword, ym)
+    url = "https://api.doorkeeper.jp/events/?q=#{keyword}&sort=starts_at#{ym.nil? ? "" : "&since=#{ym}01000000"}&page=#{start.to_s}"
     result = Shule::Http.get_json(url)
     events = result.map {|event| DoorkeeperEvent.new(event[:event])}
 
