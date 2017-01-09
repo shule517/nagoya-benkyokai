671b5dec08c9c947b04f545ade5459963c91ece8
shule517 <shule517@gmail.com>
2017-01-09 18:59:39 +0900

    add: tweeted_new tweeted_tomorrow

--- db/migrate/20170109093826_add_column_events.rb
+++ db/migrate/20170109093826_add_column_events.rb
@@ -1,6 +1,6 @@
 class AddColumnEvents < ActiveRecord::Migration[5.0]
   def change
-    add_column :events, :tweeted_new, :boolean
-    add_column :events, :tweeted_tomorrow, :boolean
+    add_column :events, :tweeted_new, :boolean, default: false, null: false
+    add_column :events, :tweeted_tomorrow, :boolean, default: false, null: false
   end
 end
