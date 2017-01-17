853d5c3bfeba2fd0482ade822dcb20394dbd5e35
shule517 <shule517@gmail.com>
2017-01-18 05:47:55 +0900

    add: ng-word

--- app/services/event_collector.rb
+++ app/services/event_collector.rb
@@ -21,7 +21,7 @@ class EventCollector
     end
 
     atnd_events = Atnd.new.search(keywords, date)
-    ngwords = ["仏教", "クリスマスパーティ", "テロリスト", "国際交流パーティ", "社会人基礎力", "カウントダウンパーティー", "ARMENIAN SONGS", "ブッダ", "BrightWoman", "幸せの種まき"]
+    ngwords = ["仏教", "クリスマスパーティ", "テロリスト", "国際交流パーティ", "社会人基礎力", "カウントダウンパーティー", "ARMENIAN SONGS", "ブッダ", "BrightWoman", "幸せの種まき", "オシャレな古城", "受講料", "mana×comu"]
     atnd_events.select! {|event| ngwords.all? {|ngword| !event.title.include?(ngword)} }
     events += atnd_events
 
