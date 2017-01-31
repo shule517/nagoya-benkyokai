c29f0e0481d98fa6b568ec73c83b21d37e8750b8
unknown <>
2017-01-31 23:32:31 +0900

    update: rake event:list_delete

--- app/services/delete_twitter_list.rb
+++ app/services/delete_twitter_list.rb
@@ -2,12 +2,19 @@ require_relative './twitter_client'
 class DeleteTwitterList
   def self.call
     @twitter = TwitterClient.new
+    lists = @twitter.lists
+
     time = Time.now
     time -= (24*60*60)*7 # 1週間前
     date = time.strftime("%Y-%m-%d")
+
     events = Event.where("ended_at < '#{date}'").select(:event_id)
     events.each do |event|
-      @twitter.destroy_list("nagoya-#{event.event_id}")
+      puts event.event_id
+      list_name = "nagoya-#{event.event_id}"
+      if lists.any?{|list| list.name == list_name}
+        @twitter.destroy_list(list_name)
+      end
     end
   end
 end
