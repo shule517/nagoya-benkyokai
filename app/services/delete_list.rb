41203a412520a679a49085d061477500f13f6c71
unknown <>
2017-02-02 22:30:56 +0900

    ツイッターリスト削除処理を追加

---
+++ app/services/delete_list.rb
@@ -0,0 +1,11 @@
+require_relative './twitter_client'
+
+twitter = TwitterClient.new
+
+twitter.lists.each do |list|
+  if list.created_at.to_s < '2017-01-20'
+    puts list.name
+    twitter.destroy_list(list.id)
+  end
+  # twitter.destroy_list(list.name)
+end
