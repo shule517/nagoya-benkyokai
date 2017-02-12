28b5ccc1e66d47b5073b1cab10df044a00cf75e2
unknown <>
2017-02-13 01:08:21 +0900

    add: migrate時にツイッターリストURL/名前の初期値を設定する

--- db/migrate/20170205123806_add_twitter_list_name_to_event.rb
+++ db/migrate/20170205123806_add_twitter_list_name_to_event.rb
@@ -2,5 +2,12 @@ class AddTwitterListNameToEvent < ActiveRecord::Migration[5.0]
   def change
     add_column :events, :twitter_list_name, :string
     add_column :events, :twitter_list_url, :string
+
+    # 初期値を設定
+    Event.all.each do |event|
+      event.twitter_list_name = "nagoya-#{event.event_id}"
+      event.twitter_list_url = "https://twitter.com/nagoya_lambda/nagoya-#{event.event_id}"
+      event.save
+    end
   end
 end
