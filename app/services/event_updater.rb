4fca2923d55442b980caaf3980add1de22ab6b00
unknown <>
2017-02-13 01:44:48 +0900

    update: ツイッターリスト名を勉強会名に変更

--- app/services/event_updater.rb
+++ app/services/event_updater.rb
@@ -3,11 +3,11 @@ class EventUpdater
     def call
       collector = EventCollector.new
       events = collector.search(collect_period)
-      @twitter = TwitterClient.new
       update_db(events)
 
+      @twitter = TwitterClient.new
       lists = @twitter.lists
-      events.each do |event|
+      Event.all.each do |event|
         update_event_to_twitter(event, lists)
       end
     end
@@ -53,23 +53,27 @@ class EventUpdater
     def update_event_to_twitter(event, lists)
       description = "#{event.year}/#{event.month}/#{event.day}(#{event.wday}) #{event.title} #{event.url}"
 
-      if !lists.any? { |list| list.name == event.twitter_list_id }
+      if !lists.any? { |list| list.uri.to_s == event.twitter_list_url }
         puts "crate list: #{description}"
-        @twitter.create_list(event.twitter_list_id, description)
+        list = @twitter.create_list(event.title, description)
+        event.twitter_list_name = list.name
+        event.twitter_list_url = list.uri
+        event.save
       else
-        puts "not crate list: #{description}"
+        puts "update list: #{description}"
+        list = @twitter.update_list(event.twitter_list_url, event.title, description)
+        event.twitter_list_name = list.name
+        event.twitter_list_url = list.uri
+        event.save
       end
 
       users = []
       users.concat(event.owners)
       users.concat(event.users)
       event_users = users.map { |user| user.twitter_id }.select { |id| !id.empty? }
-      twitter_members = @twitter.list_members(event.twitter_list_id).map { |member| member.screen_name }
+      twitter_members = @twitter.list_members(event.twitter_list_url).map { |member| member.screen_name }
       add_users = event_users.select { |user| !twitter_members.include?(user) }
-
-      add_users.each do |twitter_id|
-        @twitter.add_list_member(event.twitter_list_id, twitter_id)
-      end
+      @twitter.add_list_members(event.twitter_list_url, add_users)
     end
   end
 end
