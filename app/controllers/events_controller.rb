7fb5eed32898b885f9fdd7e4d77922391ed57be3
unknown <>
2017-02-13 04:07:02 +0900

    fix: ツイッターリストボタンのURLを修正

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,5 +2,9 @@ class EventsController < ApplicationController
   def index
     today = Time.now.strftime('%Y-%m-%d')
     @events = Event.all.where(['started_at > ?', today]).order(:started_at)
+
+    @events.each do |event|
+      event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/')
+    end
   end
 end
