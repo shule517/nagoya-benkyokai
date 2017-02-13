3c4c6017a094829bf366303d6ab38680296602cd
unknown <>
2017-02-13 22:44:17 +0900

    fix: doorkeeperでToo many requestが発生したら規制解除までsleepするように修正

--- app/services/doorkeeper.rb
+++ app/services/doorkeeper.rb
@@ -14,8 +14,18 @@ class Doorkeeper
 private
   def search_core(start, keyword, ym)
     url = "https://api.doorkeeper.jp/events/?q=#{keyword}&sort=starts_at#{ym.nil? ? '' : "&since=#{ym}01000000"}&page=#{start.to_s}"
-    result = Shule::Http.get_json(url)
-    events = result.map { |event| DoorkeeperEvent.new(event[:event]) }
+    begin
+      result = Shule::Http.get_json(url)
+      if result.nil?
+        raise
+      end
+      events = result.map { |event| DoorkeeperEvent.new(event[:event]) }
+    rescue
+      puts 'sleep(301)'
+      sleep(301)
+      puts 'retry'
+      retry
+    end
 
     if events.count == 20
       events + search_core(start + 1, keyword, ym)
