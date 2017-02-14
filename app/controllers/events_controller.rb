5b30824ca46f8d8144b0a51096c1b6f13a0a0190
unknown <>
2017-02-15 04:27:39 +0900

    add: 統計ページを仮追加

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,5 +8,42 @@ class EventsController < ApplicationController
         event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/')
       end
     end
+
+  def group
+    groups = Event.group(:group_id).count.sort_by{|k,v| v}.reverse.select{|v| !v[0].nil?}
+    p groups
+    # ng = ["2546", "2532", "2462", "7403"]
+    # groups = groups.select{|group| !ng.include?(group[0])}
+    @events = []
+    groups.each do |group|
+      @events += Event.where(group_id: group[0])
+    end
+  end
+
+  def place
+    groups = Event.group(:place).count.sort_by{|k,v| v}.reverse.select{|v| !v[0].nil?}
+    p groups
+    @events = []
+    groups.each do |group|
+      @events += Event.where(place: group[0])
+    end
+  end
+
+  def owner
+    owners = Participant.where(owner: true).group(:user_id).count.sort_by{|k,v| v}.reverse
+    @events = []
+    owners.each do |owner|
+      @events += Participant.where(user_id: owner[0], owner: true).map{|v| [User.find(owner[0]), v.event] }
+    end
+  end
+
+  def user
+    users = Participant.where(owner: false).group(:user_id).count.sort_by{|k,v| v}.reverse
+    @events = []
+    users.each do |user|
+      @events += Participant.where(user_id: user[0], owner: false).map{|v| [User.find(user[0]), v.event] }
+    end
+  end
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events
+  resources :events do
+    collection do
+      get 'group'
+      get 'place'
+      get 'owner'
+      get 'user'
+    end
+  end
   root 'events#index'
 end
