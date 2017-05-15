d6fc232afb60daf9a75373358c7ae2c77326b66a
シュール <shule517@gmail.com>
2017-05-16 01:02:25 +0900

    ネームスペースとして module Api を設定

--- app/services/api/atnd_user.rb
+++ app/services/api/atnd_user.rb
@@ -1,17 +1,19 @@
 require './app/services/api/user_base'
 
-class AtndUser < UserBase
-  def find_or_create_by
-    result = User.find_atnd(atnd_id, twitter_id, facebook_id)
-    if result
-      result.atnd_id = atnd_id          if atnd_id.present?
-      result.twitter_id = twitter_id    if twitter_id.present?
-      result.facebook_id = facebook_id  if facebook_id.present?
-      result.image_url = image_url      if image_url.present? && image_url !~ /default_latent/
-      result.save
-      return result
-    else
-      return User.create(atnd_id: atnd_id, twitter_id: twitter_id, facebook_id: facebook_id, name: name, image_url: image_url)
+module Api
+  class AtndUser < UserBase
+    def find_or_create_by
+      result = User.find_atnd(atnd_id, twitter_id, facebook_id)
+      if result
+        result.atnd_id = atnd_id          if atnd_id.present?
+        result.twitter_id = twitter_id    if twitter_id.present?
+        result.facebook_id = facebook_id  if facebook_id.present?
+        result.image_url = image_url      if image_url.present? && image_url !~ /default_latent/
+        result.save
+        return result
+      else
+        return User.create(atnd_id: atnd_id, twitter_id: twitter_id, facebook_id: facebook_id, name: name, image_url: image_url)
+      end
     end
   end
 end
--- app/services/api/user_base.rb
+++ app/services/api/user_base.rb
@@ -1,21 +1,23 @@
-class UserBase
-  attr_reader :connpass_id,
-    :atnd_id,
-    :twitter_id,
-    :facebook_id,
-    :github_id,
-    :linkedin_id,
-    :name,
-    :image_url
+module Api
+  class UserBase
+    attr_reader :connpass_id,
+      :atnd_id,
+      :twitter_id,
+      :facebook_id,
+      :github_id,
+      :linkedin_id,
+      :name,
+      :image_url
 
-  def initialize(data)
-    @connpass_id = data[:connpass_id] || ''
-    @atnd_id     = data[:atnd_id]     || ''
-    @twitter_id  = data[:twitter_id]  || ''
-    @facebook_id = data[:facebook_id] || ''
-    @github_id   = data[:github_id]   || ''
-    @linkedin_id = data[:linkedin_id] || ''
-    @name        = data[:name]        || ''
-    @image_url   = data[:image_url]   || ''
+    def initialize(data)
+      @connpass_id = data[:connpass_id] || ''
+      @atnd_id     = data[:atnd_id]     || ''
+      @twitter_id  = data[:twitter_id]  || ''
+      @facebook_id = data[:facebook_id] || ''
+      @github_id   = data[:github_id]   || ''
+      @linkedin_id = data[:linkedin_id] || ''
+      @name        = data[:name]        || ''
+      @image_url   = data[:image_url]   || ''
+    end
   end
 end
