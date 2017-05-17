0c503f9abe15e39e5cf79a4ed8e3cb1ea2b85327
シュール <shule517@gmail.com>
2017-05-18 01:07:01 +0900

    2016/12のイベントが取得できることを確認

--- spec/services/search_event_service_spec.rb
+++ spec/services/search_event_service_spec.rb
@@ -1,22 +1,21 @@
 require 'rails_helper'
 
 describe SearchEventService, type: :request do
-  let(:ym) { Date.today.strftime('%Y%m') }
-  it '今月のイベント情報が取得できること', vcr: '#search' do
-    expect { SearchEventService.new.call([ym]) }.not_to raise_error
-  end
-
   it '全てのサイトからイベントが取得されていること', vcr: '#search-all' do
     events = SearchEventService.new.call(['201612'], false)
-    classes = events.map { |event| event.class }
+    classes = events.map(&:class)
     expect(classes).to include Api::Connpass::ConnpassEvent
     expect(classes).to include Api::Doorkeeper::DoorkeeperEvent
     expect(classes).to include Api::Atnd::AtndEvent
+    titles = events.map(&:title)
+    expect(titles).to include 'NGK2016B 昼の部'               # connpass
+    expect(titles).to include 'ものづくりナイト@名古屋ギークバー' # Doorkeeper
+    expect(titles).to include '名古屋アプリ開発者ミーティング'    # ATND
   end
 
-  it '５月の場合', vcr: '#search-201705' do
+  it '2017/05のDoorkeeperのイベントが取得できること', vcr: '#search-201705' do
     events = SearchEventService.new.call(['201705'], false)
-    titles = events.map { |event| event.title }
+    titles = events.map(&:title)
     expect(titles).to include '公開アップルップル社内勉強会 Vol.36' # Doorkeeper 2017-05-15
     expect(titles).to include 'WellHashアンベール（お披露目）会＠名古屋ギークバー' # Doorkeeper 2017-05-15
   end
