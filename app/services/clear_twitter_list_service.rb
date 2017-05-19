be5bd06647d1963c15450112161395a560148ee9
シュール <shule517@gmail.com>
2017-05-19 19:08:06 +0900

    ClearTwitterListServiceをリファクタリング

--- app/services/clear_twitter_list_service.rb
+++ app/services/clear_twitter_list_service.rb
@@ -1,19 +1,32 @@
 # 開催終了から一週間経ったイベントのツイッターリストを削除する
 class ClearTwitterListService
+  attr_reader :twitter, :lists
   def call
-    twitter = TwitterClient.new
-    lists = twitter.lists
-    events = Event.where('ended_at < ?', 7.day.ago)
-    events.each do |event|
-      list_name = event.twitter_list_name
-      puts "delete #{list_name}"
-      if lists.any? { |list| list[:name] == list_name }
-        begin
-          twitter.destroy_list(event.twitter_list_url)
-        rescue => e
-          puts e
-        end
-      end
+    @twitter = TwitterClient.new
+    @lists = twitter.lists
+    clear
+  end
+
+  private
+
+  def clear
+    target_events.each do |event|
+      delete_list(event) if exists?(event)
     end
+  end
+
+  def target_events
+    Event.where('ended_at < ?', 7.day.ago)
+  end
+
+  def delete_list(event)
+    puts "delete #{event.twitter_list_name}"
+    twitter.destroy_list(event.twitter_list_url)
+  rescue => e
+    puts e
+  end
+
+  def exists?(event)
+    lists.any? { |list| list[:name] == event.twitter_list_name }
   end
 end
