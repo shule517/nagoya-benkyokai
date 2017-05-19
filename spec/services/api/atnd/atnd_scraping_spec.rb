6a9cb95a1637d399007dcf6996bb0754b8915a5e
シュール <shule517@gmail.com>
2017-05-19 23:50:52 +0900

    AtndScrapingのテストケースを追加

--- spec/services/api/atnd/atnd_scraping_spec.rb
+++ spec/services/api/atnd/atnd_scraping_spec.rb
@@ -17,6 +17,10 @@ describe AtndScraping, type: :request do
       it { expect(event.limit_over?).to eq false }
       it { expect(event.logo).to eq 'https://atnd.org/event_images/0008/0890/008_original.jpg?1474957731' }
       it { expect(event.users.count).to eq event.accepted }
+      it { expect(event.group_url).to eq nil }
+      it { expect(event.group_id).to eq nil }
+      it { expect(event.group_title).to eq nil }
+      it { expect(event.group_logo).to eq nil }
     end
 
     describe '参加者の情報が取得できること', vcr: '#find-users' do
@@ -25,6 +29,8 @@ describe AtndScraping, type: :request do
         it { expect(atnd_user.atnd_id).to eq '259586' }
         it { expect(atnd_user.twitter_id).to eq 'suzukiterminal' }
         it { expect(atnd_user.facebook_id).to eq '1800036413585884' }
+        it { expect(atnd_user.github_id).to eq '' }
+        it { expect(atnd_user.linkedin_id).to eq '' }
         it { expect(atnd_user.name).to eq 's2terminal' }
         it { expect(atnd_user.image_url).to eq 'https://pbs.twimg.com/profile_images/682491356460101632/4l54CzhH_normal.jpg' }
       end
@@ -34,6 +40,8 @@ describe AtndScraping, type: :request do
         it { expect(no_social_user.atnd_id).to eq '260559' }
         it { expect(no_social_user.twitter_id).to eq '' }
         it { expect(no_social_user.facebook_id).to eq '' }
+        it { expect(no_social_user.github_id).to eq '' }
+        it { expect(no_social_user.linkedin_id).to eq '' }
         it { expect(no_social_user.name).to eq 'otama567' }
         it { expect(no_social_user.image_url).to eq 'https://atnd.org/images/icon/default_latent.png' }
       end
@@ -44,11 +52,49 @@ describe AtndScraping, type: :request do
       let(:ateam) { owners.select { |user| user.atnd_id == '224579' }.first }
       it { expect(owners.count).to eq 1 }
       it { expect(ateam.atnd_id).to eq '224579' }
-      it { expect(ateam.name).to eq 'Ateam Inc.' }
       it { expect(ateam.twitter_id).to eq '' }
       it { expect(ateam.facebook_id).to eq '935173646549635' }
+      it { expect(ateam.github_id).to eq '' }
+      it { expect(ateam.linkedin_id).to eq '' }
+      it { expect(ateam.name).to eq 'Ateam Inc.' }
       it { expect(ateam.image_url).to eq 'https://graph.facebook.com/935173646549635/picture?type=square' }
     end
+  end
+
+  describe '#catch' do
+    context 'catchが設定されている場合'
+    context 'catchが設定されていない場合'
+  end
+
+  describe '#logo' do
+    context 'logoが設定されている場合'
+    context 'logoが設定されていない場合'
+  end
+
+  describe '#get_social_id' do
+    describe '#twitter_id' do
+      context 'twitter_idが設定されている場合'
+      context 'twitter_idが設定されていない場合' # nilであること
+    end
+    describe '#facebook_id' do
+      context 'facebook_idが設定されている場合'
+      context 'facebook_idが設定されていない場合' # nilであること
+    end
+  end
+
+  describe '#users' do
+    context '画像が設定されている場合'
+    context '画像が設定されていない場合'
+    context 'httpsからはじまる場合'
+    context 'httpsからはじまらない場合'
+    context '参加者がいない場合'
+  end
+
+  describe '#owner' do
+    context 'owner_info.empty?の場合'
+    context '画像が設定されている場合'
+    context '画像が設定されていない場合'
+    context '管理者がいない場合'
   end
 
   context '管理者の画像がない場合', vcr: '#find-RFC' do
@@ -57,9 +103,9 @@ describe AtndScraping, type: :request do
     let(:tss) { owners.select { |user| user.atnd_id == '10209' }.first }
     it { expect(owners.count).to eq 1 }
     it { expect(tss.atnd_id).to eq '10209' }
-    it { expect(tss.name).to eq 'tss_ontap' }
     it { expect(tss.twitter_id).to eq '' }
     it { expect(tss.facebook_id).to eq '' }
+    it { expect(tss.name).to eq 'tss_ontap' }
     it { expect(tss.image_url).to eq 'https://atnd.org/images/icon/default_latent.png' }
   end
 
