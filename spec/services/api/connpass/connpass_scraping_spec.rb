4927be649d4457e28e83d366b46074b60704daac
シュール <shule517@gmail.com>
2017-05-21 19:35:06 +0900

    参加者、管理者の取得のテストを書いた

--- spec/services/api/connpass/connpass_scraping_spec.rb
+++ spec/services/api/connpass/connpass_scraping_spec.rb
@@ -32,7 +32,20 @@ describe ConnpassScraping, type: :request do
     it '参加者数が取得できること', vcr: '#users' do
       expect(users.count).to eq event.accepted
     end
+    context '参加者がいない場合', vcr: '#users-nothing' do
+      # よみかきの前にプログラミング！幼児向け無料体験教室 #2 https://cocomue.connpass.com/event/50505/
+      let(:event) { api.find(event_id: 50505) }
+      it '参加者数が0であること' do
+        expect(users).to be_empty
+        expect(users.size).to eq 0
+      end
+    end
+  end
 
+  describe '#user' do
+    # JXUGC #14 Xamarin ハンズオン 名古屋大会 https://jxug.connpass.com/event/30152/
+    let(:event) { api.find(event_id: 30152) }
+    let(:users) { event.users }
     context 'shuleの場合', vcr: '#users-shule' do
       let(:shule) { users.select { |user| user.connpass_id == 'shule517' }.first }
       it { expect(shule.connpass_id).to eq 'shule517' }
@@ -69,13 +82,6 @@ describe ConnpassScraping, type: :request do
         it { expect(image_url).to eq 'https://connpass.com/static/img/common/user_no_image.gif' }
       end
     end
-
-    context '参加者がいない場合', vcr: '#users-nothing' do
-      # よみかきの前にプログラミング！幼児向け無料体験教室 #2 https://cocomue.connpass.com/event/50505/
-      let(:event) { api.find(event_id: 50505) }
-      it { expect(users).to be_empty }
-      it { expect(users.size).to eq 0 }
-    end
   end
 
   describe '#owners', vcr: '#owners' do
@@ -89,26 +95,55 @@ describe ConnpassScraping, type: :request do
       expect(owners.map(&:name)).to match_array %w(田淵義人@エクセルソフト JXUG くぅ\ -\ kuxu Sophy)
     end
 
-    context '参加者ページがある場合' do
-      # it { expect(kuu.connpass_id).to eq 'Kuxumarin' }
-      # it { expect(kuu.twitter_id).to eq 'Fumiya_Kume' }
-      # it { expect(kuu.facebook_id).to eq '' }
-      # it { expect(kuu.github_id).to eq '' }
-      # it { expect(kuu.linkedin_id).to eq '' }
-      # it { expect(kuu.image_url).to eq 'https://connpass-tokyo.s3.amazonaws.com/thumbs/75/1f/751ff2dde8d0e259e4ad95c77bcda057.png' }
-    end
-    context '参加者ページがない場合' do
-      # it { expect(kuu.connpass_id).to eq 'Kuxumarin' }
-      # it { expect(kuu.twitter_id).to eq 'Fumiya_Kume' }
-      # it { expect(kuu.facebook_id).to eq '' }
-      # it { expect(kuu.github_id).to eq '' }
-      # it { expect(kuu.linkedin_id).to eq '' }
-      # it { expect(kuu.image_url).to eq 'https://connpass-tokyo.s3.amazonaws.com/thumbs/75/1f/751ff2dde8d0e259e4ad95c77bcda057.png' }
-    end
+    context '参加者ページがある場合'
+    context '参加者ページがない場合'
     context 'owner_info.empty?の場合'
-    context '画像が設定されている場合'
-    context '画像が設定されていない場合'
-    context '管理者がいない場合'
+    context '管理者がいない場合' # そんな場合あるの？
+  end
+
+  describe '#owner' do
+    let(:owners) { event.owners }
+    context '画像が設定されている場合', vcr: '#owner-image_url_exist' do
+      # JXUGC #14 Xamarin ハンズオン 名古屋大会 https://jxug.connpass.com/event/30152/
+      let(:event) { api.find(event_id: 30152) }
+      let(:owner) { owners.select { |owner| owner.connpass_id == 'ytabuchi' }.first }
+      it 'アイコンが取得できること' do
+        expect(owner.image_url).to eq 'https://connpass-tokyo.s3.amazonaws.com/thumbs/43/9e/439e3f40da4adc279636f04b71c34b26.png'
+      end
+    end
+    context '画像が設定されていない場合', vcr: '#owner-image_url_not_exist' do
+      # 機械学習 名古屋 分科会 #3 https://machine-learning.connpass.com/event/57609/
+      let(:event) { api.find(event_id: 57609) }
+      let(:owner) { owners.select { |owner| owner.connpass_id == 'kmiwa' }.first }
+      it 'NO IMAGEアイコンが取得できること' do
+        expect(owner.image_url).to eq 'https://connpass-tokyo.s3.amazonaws.com/thumbs/f2/58/f2586c61304a16daad06a4b436fbc847.png'
+      end
+    end
+    describe 'social id' do
+      context 'IDが設定されている場合', vcr: '#user-social-exist' do
+        # 機械学習 名古屋 分科会 #3 https://machine-learning.connpass.com/event/57609/
+        let(:event) { api.find(event_id: 57609) }
+        let(:owner) { owners.select { |owner| owner.connpass_id == 'antimon2' }.first }
+        it { expect(owner.connpass_id).to eq 'antimon2' }
+        it { expect(owner.twitter_id).to eq 'antimon2' }
+        it { expect(owner.facebook_id).to eq '100001520124191' }
+        it { expect(owner.github_id).to eq 'antimon2' }
+        it { expect(owner.linkedin_id).to eq '' }
+        it { expect(owner.name).to eq 'antimon2' }
+        it { expect(owner.image_url).to eq 'https://connpass-tokyo.s3.amazonaws.com/thumbs/77/f0/77f0c8eae4e9e877c1a5681a84e6e3d1.png' }
+      end
+      context 'IDが設定されていない場合', vcr: '#user-social-not_exist' do
+        # 【JISTA中部】ITストラテジスト受験ガイド in 名古屋
+        let(:event) { api.find(event_id: 55940) }
+        let(:owner) { owners.select { |owner| owner.connpass_id == 'yoshihiro_kanada' }.first }
+        it { expect(owner.connpass_id).to eq 'yoshihiro_kanada' }
+        it { expect(owner.twitter_id).to eq '' }
+        it { expect(owner.facebook_id).to eq '' }
+        it { expect(owner.github_id).to eq '' }
+        it { expect(owner.linkedin_id).to eq '' }
+        it { expect(owner.image_url).to eq 'https://connpass.com/static/img/common/user_no_image.gif' }
+      end
+    end
   end
 
   describe '#catch' do
