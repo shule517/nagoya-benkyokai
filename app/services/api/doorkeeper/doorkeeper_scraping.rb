8e06d44e10b93df6aed3ac1758594a9111869f26
シュール <shule517@gmail.com>
2017-05-22 01:42:44 +0900

    DoorkeeperScrapingのリファクタリング

--- app/services/api/doorkeeper/doorkeeper_scraping.rb
+++ app/services/api/doorkeeper/doorkeeper_scraping.rb
@@ -18,20 +18,8 @@ module Api
       end
 
       def users
-        users = []
-        participation_doc.css('.user-profile-details').each do |user|
-          name = user.css('div.user-name').children.text
-          image_url = user.css('img').attribute('src').value
-
-          social_ids = {}
-          user.css('div.user-social > a.external-profile-link').each do |social|
-            url = social.attribute('href').value
-            get_social_id(url, social_ids)
-          end
-
-          user_info = { name: name, image_url: image_url }
-          user_info.merge!(social_ids)
-          users << DoorkeeperUser.new(user_info)
+        users = participation_doc.css('.user-profile-details').map do |user|
+          DoorkeeperUser.new(user_info(user))
         end
         users.sort_by! { |user| user.twitter_id }.reverse
       rescue
@@ -40,25 +28,27 @@ module Api
       end
 
       def owners
-        owners = []
-        group_doc.css('.with-gutter > .row > div > .user-profile > .user-profile-details').each do |owner|
-          name = owner.css('.user-name').text
-          image_url = owner.css('img').attribute('src').value
-
-          social_ids = { twitter_id: nil, facebook_id: nil, github_id: nil, linkedin_id: nil }
-          owner.css('.user-social > .external-profile-link').each do |social|
-            url = social.attribute('href').value
-            get_social_id(url, social_ids)
-          end
-
-          user_info = { name: name, image_url: image_url }
-          user_info.merge!(social_ids)
-          owners << DoorkeeperUser.new(user_info)
+        owners = group_doc.css('.with-gutter > .row > div > .user-profile > .user-profile-details').map do |owner|
+          DoorkeeperUser.new(user_info(owner))
         end
         owners.sort_by! { |user| user.twitter_id }.reverse
       end
 
       private
+
+      def user_info(user)
+        name = user.css('.user-name').text
+        image_url = user.css('img').attribute('src').value
+
+        social_ids = { twitter_id: nil, facebook_id: nil, github_id: nil, linkedin_id: nil }
+        user.css('.user-social > .external-profile-link').each do |social|
+          url = social.attribute('href').value
+          get_social_id(url, social_ids)
+        end
+
+        user_info = { name: name, image_url: image_url }
+        user_info.merge!(social_ids)
+      end
 
       def get_social_id(url, social_ids)
         if url.include?('http://twitter.com/')
