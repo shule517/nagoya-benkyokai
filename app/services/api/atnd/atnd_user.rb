a3789ae14b44b6cfaa2c0e66a0eb8e66489aa5fd
シュール <shule517@gmail.com>
2017-05-23 22:40:42 +0900

    namespaceを修正

--- app/services/api/atnd/atnd_user.rb
+++ app/services/api/atnd/atnd_user.rb
@@ -1,18 +1,20 @@
 require './app/services/api/user_base'
 
 module Api
-  class AtndUser < UserBase
-    def find_or_create_by
-      result = User.find_atnd(atnd_id, twitter_id, facebook_id)
-      if result
-        result.atnd_id = atnd_id          if atnd_id.present?
-        result.twitter_id = twitter_id    if twitter_id.present?
-        result.facebook_id = facebook_id  if facebook_id.present?
-        result.image_url = image_url      if image_url.present? && image_url !~ /default_latent/
-        result.save
-        return result
-      else
-        return User.create(atnd_id: atnd_id, twitter_id: twitter_id, facebook_id: facebook_id, name: name, image_url: image_url)
+  module Atnd
+    class AtndUser < UserBase
+      def find_or_create_by
+        result = User.find_atnd(atnd_id, twitter_id, facebook_id)
+        if result
+          result.atnd_id = atnd_id          if atnd_id.present?
+          result.twitter_id = twitter_id    if twitter_id.present?
+          result.facebook_id = facebook_id  if facebook_id.present?
+          result.image_url = image_url      if image_url.present? && image_url !~ /default_latent/
+          result.save
+          return result
+        else
+          return User.create(atnd_id: atnd_id, twitter_id: twitter_id, facebook_id: facebook_id, name: name, image_url: image_url)
+        end
       end
     end
   end
