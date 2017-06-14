3422cdff737f81b151b81467703483897c833e12
シュール <shule517@gmail.com>
2017-06-14 21:10:33 +0900

    リファクタリング

--- app/services/delete_twitter_list.rb
+++ app/services/delete_twitter_list.rb
@@ -1,14 +1,18 @@
 class DeleteTwitterList
   def self.call
     @twitter = TwitterClient.new
-    lists = twitter.lists
+    @lists = twitter.lists
+    delete
+  end
+
+  private
+  attr_reader :twitter, :lists
+
+  def delete
     target_events.each do |event|
       delete_list(event) if lists.any? { |list| list.name == event.twitter_list_name }
     end
   end
-
-  private
-  attr_reader :twitter
 
   def target_events
     Event.where('ended_at < ?', 1.month.ago) # 1ヶ月前
