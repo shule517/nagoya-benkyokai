aecb3e2cc5b91336440eac2f35f26abf243a6c02
シュール <shule517@gmail.com>
2017-06-26 00:28:12 +0900

    user画面のソート順を降順にした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
     @user = User.where(twitter_id: params[:id]).first
     participants = Participant.where(user_id: @user.id)
     event_ids = participants.map { |participant| participant.event_id }
-    @events = Event.where(id: event_ids).order(:started_at)
+    @events = Event.where(id: event_ids).order(:started_at).reverse
     @events.each do |event|
       event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
     end
