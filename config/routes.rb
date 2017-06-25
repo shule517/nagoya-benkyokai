3d1e621ac330321b32b93913930d718abc2585c6
シュール <shule517@gmail.com>
2017-06-25 23:05:38 +0900

    ランキングページをRankControllerに移動

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :events do
+  resources :rank do
     collection do
       get 'group'
       get 'place'
