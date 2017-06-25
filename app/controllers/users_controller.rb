1f56b52d38e0d1e47b3293619b741010fc470389
シュール <shule517@gmail.com>
2017-06-26 00:24:45 +0900

    user画面を仮追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,11 @@
 class UsersController < ApplicationController
+  def show
+    @user = User.where(twitter_id: params[:id]).first
+    participants = Participant.where(user_id: @user.id)
+    event_ids = participants.map { |participant| participant.event_id }
+    @events = Event.where(id: event_ids).order(:started_at)
+    @events.each do |event|
+      event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   resources :rank do
     collection do
       get 'group'
