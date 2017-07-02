e3ca8916c8dbd4dc5b71547f881804d1da95615a
シュール <shule517@gmail.com>
2017-07-02 09:21:40 +0900

    リファクタリング

--- app/services/api/techplay/techplay_api.rb
+++ app/services/api/techplay/techplay_api.rb
@@ -2,11 +2,12 @@ module Api
   module Techplay
     class TechplayApi
       def search(keyword = nil)
-        keyword = URI.escape(keyword) if keyword.present?
-        url = "https://techplay.jp/event/search?keyword=#{keyword}&pref=23&tag=&from=&to=&sort=started_asc"
-        doc = Api::Http.get_document(url)
+        doc = Api::Http.get_document(request_url(keyword))
+        scraping_tags(doc)
+      end
 
-        events = doc.css('#main-content .eventlist .eventlist-right .title').map do |event_doc|
+      def scraping_tags(doc)
+        doc.css('#main-content .eventlist .eventlist-right .title').map do |event_doc|
           title = event_doc.css('a').first.text
           tags = event_doc.css('.tags > a').map do |a|
             id = a.attribute('href').text.gsub('https://techplay.jp/tag/', '')
@@ -14,7 +15,11 @@ module Api
           end
           [title, tags]
         end
-        events
+      end
+
+      def request_url(keyword)
+        keyword = URI.escape(keyword) if keyword.present?
+        url = "https://techplay.jp/event/search?keyword=#{keyword}&pref=23&tag=&from=&to=&sort=started_asc"
       end
     end
   end
