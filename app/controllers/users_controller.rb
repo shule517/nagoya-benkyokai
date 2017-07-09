11adc4a34ffbe73927f7aaf5bd412675c62de2e1
シュール <shule517@gmail.com>
2017-07-09 22:36:33 +0900

    controllerのリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(twitter_id: params[:id])
     participants = Participant.where(user_id: @user.id)
-    event_ids = participants.map { |participant| participant.event_id }
+    event_ids = participants.map(&:event_id)
     users_events = Event.where('id in (?)', event_ids)
 
     @events = users_events.where('started_at > ?', Date.today).order(:started_at).each do |event|
