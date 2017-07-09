c59a67dcfbca857146b2c54d1a86c782bfa97803
シュール <shule517@gmail.com>
2017-07-09 21:32:35 +0900

    参加する勉強会 と 過去の勉強会 に分けて表示するようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,12 @@ class UsersController < ApplicationController
     @user = User.find_by(twitter_id: params[:id])
     participants = Participant.where(user_id: @user.id)
     event_ids = participants.map { |participant| participant.event_id }
-    @events = Event.where(id: event_ids).order(:started_at).reverse
-    @events.each do |event|
+
+    @events = Event.where('id in (?) and started_at > ?', event_ids, Date.today).order(:started_at).each do |event|
+      event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
+    end
+
+    @events_histories = Event.where('id in (?) and started_at < ?', event_ids, Date.today).order(:started_at).reverse.each do |event|
       event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
     end
   end
