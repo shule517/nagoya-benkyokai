a9239f70c7fc25ebfeb38bf2bb294c8edcde7ffa
シュール <shule517@gmail.com>
2017-07-09 22:23:30 +0900

    各グループの勉強会を表示した

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,10 @@
 class GroupsController < ApplicationController
   def show
-    @user = User.find_by(twitter_id: params[:id])
-    participants = Participant.where(user_id: @user.id)
-    event_ids = participants.map { |participant| participant.event_id }
-
-    @events = Event.where('id in (?) and started_at > ?', event_ids, Date.today).order(:started_at).each do |event|
+    @events = Event.where('group_title = ? and started_at > ?', params[:id], Date.today).order(:started_at).each do |event|
       event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
     end
 
-    @events_histories = Event.where('id in (?) and started_at < ?', event_ids, Date.today).order(:started_at).reverse.each do |event|
+    @events_histories = Event.where('group_title = ? and started_at < ?', params[:id], Date.today).order(:started_at).reverse.each do |event|
       event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
     end
   end
