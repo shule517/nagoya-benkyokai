03b6c2088766b7ca7d0684f57426c7ff4c0e2583
シュール <shule517@gmail.com>
2017-07-15 22:25:35 +0900

    使われてないアクションをroutesから削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'events#index'
-  resources :events do
+  resources :events, :only => [:index] do
     collection do
       get 'tag'
     end
   end
   get '/groups/:groupname', to: 'groups#show', groupname: /.*/
   get '/users/:userid', to: 'users#show', userid: /.*/
-  resources :rank do
+
+  # おためし機能
+  resources :rank, :only => [] do
     collection do
       get 'group'
       get 'place'
