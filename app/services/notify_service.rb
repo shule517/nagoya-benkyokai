4588f68652b371e2779e9d2837fc1d2a530c1bea
シュール <shule517@gmail.com>
2017-07-22 09:37:16 +0900

    通知先のSlackが環境変数に設定いなければ、通知しないようにした

--- app/services/notify_service.rb
+++ app/services/notify_service.rb
@@ -1,10 +1,14 @@
 class NotifyService
   def call(e, text)
     trace = e.backtrace.reject { |trace| trace.include?('/app/vendor') || trace.include?('.rbenv') }.join("\n")
-    Slack.chat_postMessage text: "#{text}\n#{e.class}\n#{e.message}\n#{trace}", channel: ENV['SLACK_ERROR_CHANNEL'], username: 'lambda'
+    Slack.chat_postMessage text: "#{text}\n#{e.class}\n#{e.message}\n#{trace}", channel: error_channel, username: 'lambda' if error_channel
   end
 
   def send_message(text)
-    Slack.chat_postMessage text: text, channel: ENV['SLACK_ERROR_CHANNEL'], username: 'lambda'
+    Slack.chat_postMessage text: text, channel: error_channel, username: 'lambda' if error_channel
+  end
+
+  def error_channel
+    ENV['SLACK_ERROR_CHANNEL']
   end
 end
