b21203b49013878068931cc24385d25a08e7fc9e
3sho7mi8 <hiro.pinot@gmail.com>
2017-07-30 08:56:37 +0900

    eventのviewを大幅に変更

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,10 @@
 module EventsHelper
+
+  def display_day_format(day)
+    day.started_at.strftime("%Y/%m/%d(#{%w(日 月 火 水 木 金 土)[Time.now.wday]})") if day.present?
+  end
+
+  def link_to_google_map(place)
+    "https://maps.google.co.jp/maps?q=#{place}"
+  end
 end
