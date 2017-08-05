c236ead878b80e3aa420b3e968713b6afd2b551b
シュール <shule517@gmail.com>
2017-08-05 21:58:13 +0900

    不要なコードを削除

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,10 +1,9 @@
 class EventsController < ApplicationController
   def index
-    @events = Event.scheduled
-    @events.each do |event|
-      event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
-    end
-
+    # @events = Event.scheduled
+    # @events.each do |event|
+    #   event.twitter_list_url = event.twitter_list_url.gsub('nagoya_lambda/', 'nagoya_lambda/lists/') if event.twitter_list_url.present?
+    # end
     @events_box = Event.upcoming_events
   end
 
