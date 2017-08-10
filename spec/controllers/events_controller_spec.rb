f3295a23fd7fc8b7c5dcaca7dc4d7ae63bc4c35f
3sho7mi8 <hiro.pinot@gmail.com>
2017-08-10 13:20:03 +0900

    events_controllerの変更でspecが通らなくなっていたのを修正

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -11,13 +11,13 @@ RSpec.describe EventsController, type: :controller do
   it '過去の勉強会は表示しないこと' do
     Event.create(started_at: Time.now.yesterday)
     get :index
-    expect(assigns(:events).length).to eq 0
+    expect(assigns(:upcoming_events).length).to eq 0
   end
 
   it '今日開催の勉強会は表示すること' do
     Event.create(started_at: Time.now)
     get :index
-    expect(assigns(:events).length).to eq 1
+    expect(assigns(:upcoming_events).length).to eq 1
   end
 
   describe 'GET #tag' do
