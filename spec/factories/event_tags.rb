c4e803c4c13350397c1c350af4f2bc3cf4e755cb
shule517 <shule517@gmail.com>
2017-10-13 04:36:38 +0900

    不要なファイルを削除

--- spec/factories/event_tags.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :event_tag do
-    event nil
-    tag nil
-  end
-end
--- spec/factories/tags.rb
+++
@@ -1,6 +0,0 @@
-FactoryGirl.define do
-  factory :tag do
-    tag_id "MyString"
-    name "MyString"
-  end
-end
