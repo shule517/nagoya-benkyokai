7f3d0dcb89602cda25c80a6849cdf7287d20fac1
shule517 <shule517@gmail.com>
2019-07-13 01:19:55 +0900

    INDEXを修正

--- db/migrate/20190712161132_add_index_to_participant.rb
+++ db/migrate/20190712161132_add_index_to_participant.rb
@@ -1,8 +1,6 @@
 class AddIndexToParticipant < ActiveRecord::Migration[5.0]
   def change
     add_index :participants, :owner
-
-    add_index :events, :twitter_id
     add_index :events, :started_at
     add_index :events, :event_url
   end
